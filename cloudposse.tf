0465b4ee8335df881fe979957b425d8806a42d8e
dhruv-garg <dhruvgarg074@gmail.com>
2023-04-18 13:25:54 +0530

    Trying cloudposse module

---
+++ cloudposse.tf
@@ -0,0 +1,58 @@
+data "aws_iam_policy_document" "inline_policy" {
+  statement {
+    sid       = "VisualEditor0"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:ListStorageLensConfigurations",
+      "s3:ListAccessPointsForObjectLambda",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListAllMyBuckets",
+      "s3:ListAccessPoints",
+      "s3:ListJobs",
+      "s3:ListBucketVersions",
+      "s3:ListBucket",
+      "s3:ListMultiRegionAccessPoints",
+      "s3:ListMultipartUploadParts"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "base" {
+  statement {
+    sid = "BaseAccess"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:ListBucketVersions"
+    ]
+
+    resources = ["arn:aws:s3:::bucketname"]
+    effect    = "Allow"
+  }
+}
+
+module "role" {
+  source = "cloudposse/iam-role/aws"
+  version     = "0.17.0"
+  providers = {
+    aws = aws.dev
+  }
+
+  enabled   = true
+  name      = "TerraformTestRole-cloudposse"
+
+  policy_description = "Testing Cloudposse"
+  role_description   = "IAM role with permissions to perform actions on S3 resources"
+
+  assume_role_actions = ["sts:AssumeRole"]
+  principals = {
+    Service = ["ec2.amazonaws.com"]
+  }
+
+  policy_documents = [
+    data.aws_iam_policy_document.inline_policy.json,
+    data.aws_iam_policy_document.base.json
+  ]
+}
