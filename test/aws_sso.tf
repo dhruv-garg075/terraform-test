951345926ad6363309563e83496455c7cb59a9b7
dhruv-garg <dhruvgarg074@gmail.com>
2023-05-03 18:25:41 +0530

    Add iam user

---
+++ test/aws_sso.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_policy" "list_roles_policy" {
+  name        = "ListRoles"
+  path        = "/"
+  description = "Policy to list all IAM roles"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "iam:ListRoles",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_user" "azuread_provisioner" {
+  name = "AzureAD-Provisioner"
+}
+
+resource "aws_iam_user_policy_attachment" "azuread_policy_attach" {
+  user       = aws_iam_user.azuread_provisioner.name
+  policy_arn = aws_iam_policy.list_roles_policy.arn
+}
+
+resource "aws_iam_access_key" "azuread" {
+  user    = aws_iam_user.azuread_provisioner.name
+  pgp_key = "keybase:elocal"
+}
+
+output "secret" {
+  value = aws_iam_access_key.azuread.encrypted_secret
+}
