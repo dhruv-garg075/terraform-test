d96990d76248af0781edec8813e5f6431aa9b67a
dhruv-garg <dhruvgarg074@gmail.com>
2023-05-04 14:24:55 +0530

    add provider

--- test/role.tf
+++ test/role.tf
@@ -1,9 +1,11 @@
 resource "aws_iam_role" "test_service_role" {
+  provider = aws.prod
   name = "TestServiceRole"
   assume_role_policy = file("policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "test_policy_attach" {
+   provider = aws.prod
    role       = "${aws_iam_role.test_service_role.name}"
    for_each = toset([
      "arn:aws:iam::430288206927:policy/ALBIngressControllerIAMPolicy",
